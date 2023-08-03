b9e651a280de073f0bb1b70ed5220fc777977768
tantanGH <tantangh68@gmail.com>
2023-08-03 18:17:39 +0900

    debug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rssnd",
-    version="0.6.0e1",
+    version="0.6.0",
     author="tantanGH",
     author_email="tantanGH@github",
     license='MIT',
